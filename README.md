c8b3050f2facf6a828a8cdce7acb48c9be301602
bklevence <brandonklevence@gmail.com>
2025-08-19 21:38:57 -0400

    readme, code.py, and new bmps
    Updated readme, code.py is my current working version for redline porter towards dtx, added bmps for green and red lines.

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # MBTA Matrix Portal 
 > Train scheduler board that displays Subway/Metro/Trains/Bus, next arrival schedules on real time based. -Enrique Gamboa @egamboafuentes . 
 
+## Overview
+
+I was compelled to start using my MatrixPortal again. I previously used it as a [countdown to holidays](https://learn.adafruit.com/halloween-countdown-display-matrix) throughout the year in my classroom, however now as an at home caretaker for my kiddo I wanted to have it as something useful for my partner who takes the MBTA daily. I found Enrique's work, but it wasn't working correctly with my setup. So I updated everything and tinkered until I got something working again that matched my [MBTA stops webpage](https://www.mbta.com/stops/place-portr). See below for more working notes in [WIP Status](#WIP-Status). For a better writeup on how to successfully tune this to your MBTA line, stop, and travel direction check out [Enrique's Medium post](https://jegamboafuentes.medium.com/i-created-my-own-subway-arrival-board-with-real-time-data-to-dont-miss-my-train-anymore-28bfded312c0?source=friends_link&sk=a229cfebc19bc9f1874ba3a0441f0620). I may come back in the future to add that content here, but for now I'm just looking to get things working! :sunglasses:
+
 > [!IMPORTANT]
 > You will want a settings.toml file to go along with the code.py. TL;DR: it's the better version of secrets.py. It will need to contain the following:
 
@@ -23,10 +27,16 @@ TIMEZONE = "America/New_York", # http://worldtimeapi.org/timezones
         - [NINA_ADAFRUIT-esp32-3.1.0.bin](https://github.com/adafruit/nina-fw/releases/tag/3.1.0)
 - [x] Update Libraries in /lib directory.
     - [x] [adafruit-circuitpython-bundle-9.x-mpy-20250810.zip](https://circuitpython.org/libraries)
-- [ ] Get code.py on updated board.
+- [x] Get [code.py](code.py) on updated board.
 - [ ] Debug.
+    - [x] Fixed json memory allocation overload on second update.
+    - [ ] Possible memory allocation issue over time(hrs) or json access overload.
+        - [ ] Include reboot after x hours if this is a trend.
+- [ ] Instructions on how to setup
 
  ## Links of interest
 - [Adafruit Matrix Portal M4](https://www.adafruit.com/product/4745)
-- [Original Medium writeup by Enrique I found.](https://jegamboafuentes.medium.com/i-created-my-own-subway-arrival-board-with-real-time-data-to-dont-miss-my-train-anymore-28bfded312c0?source=friends_link&sk=a229cfebc19bc9f1874ba3a0441f0620)
--  [Starting Point for this forked project.](https://github.com/jegamboafuentes/Train_schedule_board/tree/main/display_code/8-23-23/new)
+- [Original Medium Post](https://jegamboafuentes.medium.com/i-created-my-own-subway-arrival-board-with-real-time-data-to-dont-miss-my-train-anymore-28bfded312c0?source=friends_link&sk=a229cfebc19bc9f1874ba3a0441f0620)
+- [Upstream repository (ae2938fc52b9993b929b04ba700fe58fa47946cb) for my work here](https://github.com/jegamboafuentes/Train_schedule_board/tree/main/display_code/8-23-23/new)
+- [MBTA Stops Webpage](https://www.mbta.com/stops/subway)
+- [MBTA V3 API](https://www.mbta.com/developers/v3-api)
--- code.py
+++ code.py
@@ -1,135 +1,170 @@
-#APRIL 1 2022
-#Jorge Enrique Gamboa Fuentes
-#Subway schedule board - single direction
-#Data from: Boston - MBTA
-# .l. - .l. - .l. - .l. - .l. - .l. - .l. - .l. - .l. - .l. - .l. - .l. - .l. - .l. - .l. - .l. -
+# MBTA Adafruit Matrix Portal M4 Display
+# Updated for CircuitPython 9.x.x
+# Based on original work Train_schedule_board by Jorge Enrique Gamboa Fuentes
+# Vibe coded into working after upgrade of circuitpython and wifi/ble drivers by bk @bklevence on github
+# Data from: Boston - MBTA
+# 
+# Note 
+# I've kept this running for about 6 hours and noticed a memory allocation issue maybe once 
+# Have not had time to recreate with a serial connection to PC to log errors
+# I may come back to include a restart every few hours, but this is better than my first iteration 
 
 import time
 import microcontroller
-from board import NEOPIXEL
+import gc
+import json
+import board
 import displayio
 import adafruit_display_text.label
+import os
 from adafruit_datetime import datetime
 from adafruit_bitmap_font import bitmap_font
 from adafruit_matrixportal.matrix import Matrix
 from adafruit_matrixportal.network import Network
-import json
 
-#CONFIGURABLE PARAMETERS
-#-*-/-*-\-*--*-/-*-\-*--*-/-*-\-*--*-/-*-\-*--*-/-*-\-*--*-/-*-\-*--*-/-*-\-*--*-/-*-\-*-
-BOARD_TITLE = 'Bowdoin'
-STOP_ID = 'place-wondl'
+
+# --- CONFIGURABLE PARAMETERS ---
+# I may do an overview of these, but for now see below
+# https://jegamboafuentes.medium.com/i-created-my-own-subway-arrival-board-with-real-time-data-to-dont-miss-my-train-anymore-28bfded312c0
+BOARD_TITLE = 'Porter'
+STOP_ID = 'place-portr'
 DIRECTION_ID = '0'
-ROUTE = 'Blue'
-BACKGROUND_IMAGE = 'TBlue-dashboard.bmp'
+ROUTE = 'Red'
+BACKGROUND_IMAGE = 'Tred-dashboard.bmp'
 PAGE_LIMIT = '3'
-DATA_SOURCE = 'https://api-v3.mbta.com/predictions?filter%5Bstop%5D='+STOP_ID+'&filter%5Bdirection_id%5D='+DIRECTION_ID+'&page%5Blimit%5D='+PAGE_LIMIT+'&sort=departure_time'
+DATA_SOURCE = f'https://api-v3.mbta.com/predictions?filter%5Bstop%5D={STOP_ID}&filter%5Bdirection_id%5D={DIRECTION_ID}&page%5Blimit%5D={PAGE_LIMIT}&sort=departure_time'
 UPDATE_DELAY = 15
 SYNC_TIME_DELAY = 30
-MINIMUM_MINUTES_DISPLAY = 9
 ERROR_RESET_THRESHOLD = 3
-#-*-/-*-\-*--*-/-*-\-*--*-/-*-\-*--*-/-*-\-*--*-/-*-\-*--*-/-*-\-*--*-/-*-\-*--*-/-*-\-*-
+
+
+# Rotation setting - 0, 90, 180, or 270 degrees clockwise
+# Useful as I needed to put this on my fridge
+DISPLAY_ROTATION = 180
+# --- END CONFIGURABLE PARAMETERS ---
 
 def get_arrival_in_minutes_from_now(now, date_str):
-    train_date = datetime.fromisoformat(date_str).replace(tzinfo=None) # Remove tzinfo to be able to diff dates
-    return round((train_date-now).total_seconds()/60.0)
+    """Calculates minutes from now to a given time."""
+    try:
+        train_date = datetime.fromisoformat(date_str).replace(tzinfo=None)
+        return round((train_date - now).total_seconds() / 60.0)
+    except (ValueError, TypeError):
+        return -999
 
-def get_arrival_times():
+def get_arrival_times_optimized():
+    """Fetches and parses train arrival times with memory efficiency."""
     now = datetime.now()
-    print(now)
-    print("Data source: "+DATA_SOURCE)
-    stop_trains =  network.fetch_data(DATA_SOURCE)
-    res = json.loads(stop_trains)
+    print(f"Current time: {now}")
+    print(f"Fetching from: {DATA_SOURCE}")
+
+    predictions = []
     try:
-        train1 = res["data"][0]["attributes"]["departure_time"]
-        train1_min = get_arrival_in_minutes_from_now(now, train1) - 1
-    except:
-        train1_min=-999
-    try:
-        train2 = res["data"][1]["attributes"]["departure_time"]
-        train2_min = get_arrival_in_minutes_from_now(now, train2) - 1
-    except:
-        train2_min=-888
-    try:
-        train3 = res["data"][2]["attributes"]["departure_time"]
-        train3_min = get_arrival_in_minutes_from_now(now, train3) - 1
-    except:
-        train3_min=-777
+        # Use fetch instead of get for libraries etc
+        response = network.fetch(DATA_SOURCE)
+        data = response.json()
+        response.close()
 
-    return train1_min,train2_min,train3_min
+        for prediction in data.get("data", []):
+            departure_time = prediction.get("attributes", {}).get("departure_time")
+            if departure_time:
+                predictions.append(departure_time)
 
-def text_formating(trainMinutes):
-    textFormated = ""
-    if(trainMinutes<0&trainMinutes<-700):
-        trainMinutes = 0
-    if(trainMinutes<10):
-        textFormated = "%s  min" % (trainMinutes)
-    else:
-        textFormated = "%s min" % (trainMinutes)
-    if(trainMinutes<=0):
-        textFormated = "brding"
+        del data
+        gc.collect()
 
-    if(trainMinutes==-777):
-        textFormated = "-----"
-    elif(trainMinutes==-888):
-        textFormated = "-----"
-    elif(trainMinutes==-999):
-        textFormated = "-----"
+    except (RuntimeError, ValueError) as e:
+        print(f"Network or JSON parsing error: {e}")
+        return (-999, -888, -777)
 
-    return textFormated
+    train_minutes = [get_arrival_in_minutes_from_now(now, p_time) - 1 for p_time in predictions]
 
+    while len(train_minutes) < 3:
+        train_minutes.append(-777)
 
-def update_text(t1, t2, t3):
+    return tuple(train_minutes[:3])
 
-    if(text_formating(t1)=="brding"):
-        text_lines[2].color =  colors[2]
-    else:
-        text_lines[2].color =  colors[1]
+def format_text(minutes):
+    """Formats the time in minutes into a display string."""
+    if minutes < 0:
+        if minutes in [-999, -888, -777]:
+            return "----"
+        else:
+            return "brding"
+    if minutes <= 0:
+        return "brding"
+    if minutes < 10:
+        return f"{minutes}  min"
+    return f"{minutes} min"
 
-    text_lines[2].text = text_formating(t1)
-    text_lines[3].text = text_formating(t2)
-    text_lines[4].text = text_formating(t3)
+def update_display_text(t1, t2, t3):
+    """Updates the text labels on the display."""
+    text_lines[1].text = format_text(t1)
+    text_lines[2].text = format_text(t2)
+    text_lines[3].text = format_text(t3)
+    text_lines[1].color = colors[2] if format_text(t1) == "brding" else colors[1]
+    display.root_group = group   # corrected for updated libraries
 
-    display.show(group)
-
-# --- Display setup ---
-matrix = Matrix()
+# --- Main setup, runs once ---
+matrix = Matrix(rotation=DISPLAY_ROTATION)
 display = matrix.display
-network = Network(status_neopixel=NEOPIXEL, debug=False)
-
-# --- Drawing setup ---
+network = Network(status_neopixel=board.NEOPIXEL, debug=False)
 group = displayio.Group()
-bitmap = displayio.OnDiskBitmap(open(BACKGROUND_IMAGE, 'rb'))
-colors = [0x444444, 0xDD8000, 0x9966cc]  # [dim white, gold]
+colors = [0x444444, 0xDD8000, 0x9966cc]
+
+tile_grid = None
+try:
+    if BACKGROUND_IMAGE:
+        bitmap = displayio.OnDiskBitmap(open(BACKGROUND_IMAGE, "rb"))
+        tile_grid = displayio.TileGrid(bitmap, pixel_shader=getattr(bitmap, 'pixel_shader', displayio.ColorConverter()))
+except (OSError, ValueError) as e:
+    print(f"Could not load background image '{BACKGROUND_IMAGE}': {e}")
+    bitmap = displayio.Bitmap(64, 32, 1)
+    palette = displayio.Palette(1)
+    palette[0] = 0x000000
+    tile_grid = displayio.TileGrid(bitmap, pixel_shader=palette)
+
+if tile_grid:
+    group.append(tile_grid)
 
 font = bitmap_font.load_font("fonts/6x10.bdf")
+
 text_lines = [
-    displayio.TileGrid(bitmap, pixel_shader=getattr(bitmap, 'pixel_shader', displayio.ColorConverter())),
     adafruit_display_text.label.Label(font, color=colors[0], x=20, y=3, text=BOARD_TITLE),
     adafruit_display_text.label.Label(font, color=colors[1], x=26, y=11, text="- min"),
     adafruit_display_text.label.Label(font, color=colors[1], x=26, y=20, text="- min"),
     adafruit_display_text.label.Label(font, color=colors[1], x=26, y=28, text="- min"),
 ]
-for x in text_lines:
-    group.append(x)
-display.show(group)
+
+for line in text_lines:
+    group.append(line)
+
+display.root_group = group   # corrected for updated libraries
 
 error_counter = 0
 last_time_sync = None
+print("Starting MBTA display...")
+gc.collect()
+print(f"Free memory: {gc.mem_free()} bytes")
+
+# --- Main loop ---
 while True:
     try:
         if last_time_sync is None or time.monotonic() > last_time_sync + SYNC_TIME_DELAY:
-            # Sync clock to minimize time drift
             network.get_local_time()
             last_time_sync = time.monotonic()
-        #print("-*--> ENRIQUE HERE")
-        #print(get_arrival_times())
-        arrivals = get_arrival_times()
-        update_text(*arrivals)
-    except (ValueError, RuntimeError) as e:
-        print("Some error occured, retrying! -", e)
-        error_counter = error_counter + 1
+            gc.collect()
+
+        arrivals = get_arrival_times_optimized()
+        update_display_text(*arrivals)
+        error_counter = 0
+
+    except Exception as e:
+        print(f"An error occurred: {e}. Retrying...")
+        error_counter += 1
+        gc.collect()
+
         if error_counter > ERROR_RESET_THRESHOLD:
+            print("Too many errors. Resetting microcontroller...")
             microcontroller.reset()
 
     time.sleep(UPDATE_DELAY)
